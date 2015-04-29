da03a17e41b1c3679691dc8b98ca4aef2dc9db63
Shane Prendergast <contact@webknit.co.uk>
2015-04-29 23:02:49 +0100

    starting fresh with framework

--- app/js/app.js
+++ app/js/app.js
@@ -36,13 +36,6 @@ angular.module('F1FeederApp.services', []).
 
 // initialize our app and register the modules on which it depends
 angular.module('F1FeederApp', [
-	'F1FeederApp.controllers',
-	'F1FeederApp.services',
-	'ngRoute'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-	when("/drivers", {templateUrl: "partials/drivers.html", controller: "driversController"}).
-	when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
-	otherwise({redirectTo: '/drivers'});
-}]);
+  'F1FeederApp.services',
+  'F1FeederApp.controllers'
+]);
